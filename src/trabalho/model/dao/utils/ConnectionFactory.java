7c1c8bf8fb015bab6b1944cc090309bb830065b7
Gabriel-Wojciechowski <gabrielwo341@gmail.com>
2021-07-24 16:07:38 -0300

    start_env

--- src/trabalho/model/dao/utils/ConnectionFactory.java
+++ src/trabalho/model/dao/utils/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+public class ConnectionFactory implements AutoCloseable {
 
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String URL = "jdbc:postgresql://localhost:5432/java";
@@ -30,5 +30,18 @@ public class ConnectionFactory {
             }
         }
         return con;
+    }
+    
+    @Override
+    public void close() {
+        if (con != null) {
+            try {
+                con.close();
+                con = null;
+            } catch (Exception e) {
+                System.out.println("Erro fechando a conexão. IGNORADO");
+                e.printStackTrace();
+            }
+        }
     }
 }
